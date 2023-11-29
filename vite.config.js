14e4ff9954432000e604f7546e7c52dd52740844
lrbecerrab <lrbecerrab@unal.edu.co>
2023-11-28 19:32:53 -0500

    Update docker-compose.yaml and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: "0.0.0.0",
+    strictPort: true,
     port: 5183,
   },
 });
