b62758f3b703768f1b71d0beea2d050f0ef43a2d
lrbecerrab <lrbecerrab@unal.edu.co>
2024-01-21 10:53:39 -0500

    Add Setting page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Financial } from "./Financial";
 export { default as Market } from "./Market";
 export { default as Prosumer } from "./Prosumer";
+export { default as Setting } from "./Setting";
 export { default as Welcome } from "./Welcome";
